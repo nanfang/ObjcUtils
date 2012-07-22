c202c492c3532685e718000384cb3b637f309afb
Fang Nan <nanfang05@gmail.com>
2012-07-22 16:53:19 +0800

    init project

---
+++ ObjcUtils/CommonUtils.m
@@ -0,0 +1,13 @@
+//
+//  CommonUtils.m
+//  ObjcUtils
+//
+//  Created by nanfang on 7/22/12.
+//  Copyright (c) 2012 tukeQ.com. All rights reserved.
+//
+
+#import "CommonUtils.h"
+
+@implementation CommonUtils
+
+@end
---
+++ ObjcUtils/DBUtils.h
@@ -0,0 +1,13 @@
+//
+//  DBUtils.h
+//  ObjcUtils
+//
+//  Created by nanfang on 7/22/12.
+//  Copyright (c) 2012 tukeQ.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBUtils : NSObject
+
+@end
